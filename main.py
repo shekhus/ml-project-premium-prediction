502a8987cf7c4c9a2c9dc0c70c0f81c3f4a2c5e0
Chandrashekhar Satav <cpsatav@gmail.com>
2025-07-30 23:02:55 +0530

    Define the expected columns and initialize the DataFrame with zeros

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ from prediction_helper import predict
 # Define the page layout
 st.title('Health Insurance Cost Predictor')
 
+categorical_options = {
+    'Gender': ['Male', 'Female'],
+    'Marital Status': ['Unmarried', 'Married'],
+    'BMI Category': ['Normal', 'Obesity', 'Overweight', 'Underweight'],
+    'Smoking Status': ['No Smoking', 'Regular', 'Occasional'],
+    'Employment Status': ['Salaried', 'Self-Employed', 'Freelancer', ''],
+    'Region': ['Northwest', 'Southeast', 'Northeast', 'Southwest'],
+    'Medical History': [
+        'No Disease', 'Diabetes', 'High blood pressure', 'Diabetes & High blood pressure',
+        'Thyroid', 'Heart disease', 'High blood pressure & Heart disease', 'Diabetes & Thyroid',
+        'Diabetes & Heart disease'
+    ],
+    'Insurance Plan': ['Bronze', 'Silver', 'Gold']
+}
 
 
 
--- prediction_helper.py
+++ prediction_helper.py
@@ -9,6 +9,17 @@ scaler_young = joblib.load("artifacts/scaler_young.joblib")
 scaler_rest = joblib.load("artifacts/scaler_rest.joblib")
 
 
+def preprocess_input(input_dict):
+    # Define the expected columns and initialize the DataFrame with zeros
+    expected_columns = [
+        'age', 'number_of_dependants', 'income_lakhs', 'insurance_plan', 'genetical_risk', 'normalized_risk_score',
+        'gender_Male', 'region_Northwest', 'region_Southeast', 'region_Southwest', 'marital_status_Unmarried',
+        'bmi_category_Obesity', 'bmi_category_Overweight', 'bmi_category_Underweight', 'smoking_status_Occasional',
+        'smoking_status_Regular', 'employment_status_Salaried', 'employment_status_Self-Employed'
+    ]
+
+    insurance_plan_encoding = {'Bronze': 1, 'Silver': 2, 'Gold': 3}
+
 
 
 def predict(input_dict):
