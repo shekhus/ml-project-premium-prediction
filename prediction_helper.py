5180fd5afaaa96fee81146ca7d8fc7b2187414ae
Chandrashekhar Satav <cpsatav@gmail.com>
2025-07-30 23:03:31 +0530

    Manually assign values for each categorical input based on input_dict

--- prediction_helper.py
+++ prediction_helper.py
@@ -20,7 +20,72 @@ def preprocess_input(input_dict):
 
     insurance_plan_encoding = {'Bronze': 1, 'Silver': 2, 'Gold': 3}
 
+    df = pd.DataFrame(0, columns=expected_columns, index=[0])
+    # df.fillna(0, inplace=True)
 
+    # Manually assign values for each categorical input based on input_dict
+    for key, value in input_dict.items():
+        if key == 'Gender' and value == 'Male':
+            df['gender_Male'] = 1
+        elif key == 'Region':
+            if value == 'Northwest':
+                df['region_Northwest'] = 1
+            elif value == 'Southeast':
+                df['region_Southeast'] = 1
+            elif value == 'Southwest':
+                df['region_Southwest'] = 1
+        elif key == 'Marital Status' and value == 'Unmarried':
+            df['marital_status_Unmarried'] = 1
+        elif key == 'BMI Category':
+            if value == 'Obesity':
+                df['bmi_category_Obesity'] = 1
+            elif value == 'Overweight':
+                df['bmi_category_Overweight'] = 1
+            elif value == 'Underweight':
+                df['bmi_category_Underweight'] = 1
+        elif key == 'Smoking Status':
+            if value == 'Occasional':
+                df['smoking_status_Occasional'] = 1
+            elif value == 'Regular':
+                df['smoking_status_Regular'] = 1
+        elif key == 'Employment Status':
+            if value == 'Salaried':
+                df['employment_status_Salaried'] = 1
+            elif value == 'Self-Employed':
+                df['employment_status_Self-Employed'] = 1
+        elif key == 'Insurance Plan':  # Correct key usage with case sensitivity
+            df['insurance_plan'] = insurance_plan_encoding.get(value, 1)
+        elif key == 'Age':  # Correct key usage with case sensitivity
+            df['age'] = value
+        elif key == 'Number of Dependants':  # Correct key usage with case sensitivity
+            df['number_of_dependants'] = value
+        elif key == 'Income in Lakhs':  # Correct key usage with case sensitivity
+            df['income_lakhs'] = value
+        elif key == "Genetical Risk":
+            df['genetical_risk'] = value
+
+    # Assuming the 'normalized_risk_score' needs to be calculated based on the 'age'
+    df['normalized_risk_score'] = calculate_normalized_risk(input_dict['Medical History'])
+    df = handle_scaling(input_dict['Age'], df)
+
+    return df
+
+def handle_scaling(age, df):
+    # scale age and income_lakhs column
+    if age <= 25:
+        scaler_object = scaler_young
+    else:
+        scaler_object = scaler_rest
+
+    cols_to_scale = scaler_object['cols_to_scale']
+    scaler = scaler_object['scaler']
+
+    df['income_level'] = None # since scaler object expects income_level supply it. This will have no impact on anything
+    df[cols_to_scale] = scaler.transform(df[cols_to_scale])
+
+    df.drop('income_level', axis='columns', inplace=True)
+
+    return df
 
 def predict(input_dict):
     input_df = preprocess_input(input_dict)
